5beaaaef58a8af3ce1cacb9a3bc3afdbc1561cb8
Salah-20 <selqaddoury01@educarex.es>
2023-12-05 13:07:24 +0100

    modificado

--- src/UsoArrays.java
+++ src/UsoArrays.java
@@ -1,9 +1,36 @@
 
+
+import javax.swing.JOptionPane;
+
 public class UsoArrays {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  public static void main(String[] args) {
+   // TODO Esbozo de método generado automáticamente
 
+   int opcion=Integer.parseInt(JOptionPane.showInputDialog("Dame una opción (1-2)"));
+
+   if(opcion==1) {
+
+	Arrays miArray=new Arrays(5);
+
+	miArray.getDatosArray();
+
+			
+   }else {
+	if(opcion==2) {
+				
+	 int cont=Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos nombres quieres introducir?"));
+	 VectorNombres vNombre=new VectorNombres(cont);
+				
+	  for (int i=0;i<cont;i++) {
+		String nom=JOptionPane.showInputDialog("Dame un nombre:");
+		vNombre.setGuardarNombre(nom);
+	  }
+				
+         vNombre.getNombres();
 	}
+    }
 
+  }
 }
+
